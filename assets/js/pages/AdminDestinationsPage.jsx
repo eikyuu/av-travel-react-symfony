cd261ff1f35849a2710590bccd70a8d33c1bc1b6
eikyuu <v.duguet@laposte.net>
2020-06-02 00:13:05 +0200

    paginated component

--- assets/js/pages/AdminDestinationsPage.jsx
+++ assets/js/pages/AdminDestinationsPage.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import destinationsApi from "../services/destinationsApi";
+import Pagination from "../components/Pagination";
 
 const AdminDestinationsPage = () => {
   const [destinations, setDestinations] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const fetchDestinations = async () => {
     try {
@@ -28,45 +30,59 @@ const AdminDestinationsPage = () => {
       setDestinations(originalDestinations);
     }
   };
+
+  // Gestion du changement de page
+  const handlePageChange = (page) => setCurrentPage(page);
+  const itemsPerPage = 5;
+  const start = currentPage * itemsPerPage - itemsPerPage;
+  const paginatedDestinations = destinations.slice(start, start + itemsPerPage);
   return (
     <>
       <div className="container mt-3">
         <h1>Liste des destinations</h1>
-      </div>
-      <table className="table table-hover container">
-        <thead>
-          <tr>
-            <th>Id.</th>
-            <th>Titre</th>
-            <th>Description</th>
-            <th>Pays</th>
-            <th>Ville</th>
-            <th>Image</th>
-            <th />
-          </tr>
-        </thead>
 
-        <tbody>
-          {destinations.map((destination) => (
-            <tr key={destination.id}>
-              <td>{destination.id}</td>
-              <td>{destination.title}</td>
-              <td>{destination.description}</td>
-              <td>{destination.pays}</td>
-              <td>{destination.city}</td>
-              <td>{destination.image}</td>
-              <td>
-                <button
-                  onClick={() => handleDelete(destination.id)}
-                  className="btn btn-sm btn-danger"
-                >
-                  Supprimer
-                </button>
-              </td>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Id.</th>
+              <th>Titre</th>
+              <th>Description</th>
+              <th>Pays</th>
+              <th>Ville</th>
+              <th>Image</th>
+              <th />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {paginatedDestinations.map((destination) => (
+              <tr key={destination.id}>
+                <td>{destination.id}</td>
+                <td>{destination.title}</td>
+                <td>{destination.description}</td>
+                <td>{destination.pays}</td>
+                <td>{destination.city}</td>
+                <td>{destination.image}</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(destination.id)}
+                    className="btn btn-sm btn-danger"
+                  >
+                    Supprimer
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <Pagination
+          currentPage={currentPage}
+          itemsPerPage={itemsPerPage}
+          length={destinations.length}
+          onPageChanged={handlePageChange}
+        />
+      </div>
     </>
   );
 };
