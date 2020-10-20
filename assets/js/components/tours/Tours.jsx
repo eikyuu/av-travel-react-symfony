db5191e04f9d52fa13b683f1aff20ea3e81119e6
eikyuu <v.duguet@laposte.net>
2020-10-20 16:47:59 +0200

    reponsive correctif toursCard

--- assets/js/components/tours/Tours.jsx
+++ assets/js/components/tours/Tours.jsx
@@ -41,7 +41,10 @@ const Tours = (props) => {
             .reverse()
             .slice(0, 6)
             .map((tours) => (
-              <div key={tours.id} className="mt-3 col-sm-6 col-md-6 col-xl-4">
+              <div
+                key={tours.id}
+                className="mt-3 col-12 col-md-6 col-xl-4 displayTours"
+              >
                 <ToursCards
                   id={tours.id}
                   image={tours.image}
