76fa131ec63075e95d4fb25ee1f595478e0b79be
eikyuu <v.duguet@laposte.net>
2020-12-19 22:51:34 +0100

    add imageGrid page detail destination and croisiere and update packagejson

--- assets/js/pages/detailDestination/DetailDestination.jsx
+++ assets/js/pages/detailDestination/DetailDestination.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, lazy } from "react";
 import destinationsApi from "../../services/destinationsApi";
 import "./DetailDestination.css";
 import { Link } from "react-router-dom";
 import ErrorBoundary from "../../components/ErrorBoundary";
+const ImageGrid = lazy(() => import("../../components/loaders/ImageGrid"));
 
 const DetailDestination = ({ match }) => {
   const { id } = match.params;
+  const [loading, setLoading] = useState(true);
   const [destinations, setDestinations] = useState({
     title: "",
     description: "",
@@ -25,6 +27,7 @@ const DetailDestination = ({ match }) => {
         tours,
       } = await destinationsApi.find(id);
       setDestinations({ title, description, country, city, image, tours });
+      setLoading(false);
     } catch (error) {
       toast.error("La destination n'a pas pu être chargé");
     }
@@ -38,18 +41,25 @@ const DetailDestination = ({ match }) => {
     <ErrorBoundary>
       <section className="container detailDestination">
         <div className="row detailDestination_row">
-          <h1 className="detailDestination_h1 col-12">{destinations.title}</h1>
-          <img
-            className="col-sm-12 col-xl-6 mt-3 mb-3"
-            src={destinations.image}
-            alt="image destination"
-          />
-          <p className="detailDestination_p col-sm-12 col-xl-6 mt-2">
-            {destinations.description}
-          </p>
-          <p className="detailDestination_p col-sm-12 col-xl-6">
-            {destinations.city} {destinations.country}
-          </p>
+          {!loading && (
+            <>
+              <h1 className="detailDestination_h1 col-12">
+                {destinations.title}
+              </h1>
+              <img
+                className="col-sm-12 col-xl-6 mt-3 mb-3"
+                src={destinations.image}
+                alt="image destination"
+              />
+              <p className="detailDestination_p col-sm-12 col-xl-6 mt-2">
+                {destinations.description}
+              </p>
+              <p className="detailDestination_p col-sm-12 col-xl-6">
+                {destinations.city} {destinations.country}
+              </p>
+            </>
+          )}
+          {loading && <ImageGrid />}
         </div>
 
         <Link
--- assets/js/pages/detailTours/DetailTours.jsx
+++ assets/js/pages/detailTours/DetailTours.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, lazy } from "react";
 import { toast } from "react-toastify";
 import toursApi from "../../services/toursApi";
 import JwtDecode from "jwt-decode";
@@ -6,6 +6,7 @@ import bookingApi from "../../services/bookingApi";
 import authContext from "../../contexts/authContext";
 import ReactDatePicker from "react-datepicker";
 import "./DetailTours.css";
+const ImageGrid = lazy(() => import("../../components/loaders/ImageGrid"));
 
 import "react-datepicker/dist/react-datepicker.css";
 import ErrorBoundary from "../../components/ErrorBoundary";
@@ -14,6 +15,7 @@ const DetailTours = ({ match, history }) => {
   const { id } = match.params;
   const { isAuthenticated, setIsAuthenticated } = useContext(authContext);
   const [tours, setTours] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const findUser = () => {
     let result;
@@ -37,6 +39,7 @@ const DetailTours = ({ match, history }) => {
     try {
       const data = await toursApi.find(id);
       setTours(data);
+      setLoading(false);
     } catch (error) {
       toast.error("Le tours n'a pas pu être chargé");
       props.history.replace("/profile");
@@ -66,29 +69,33 @@ const DetailTours = ({ match, history }) => {
   return (
     <ErrorBoundary>
       <section className="container mb-5 detailTours_container">
-        <div className="row">
-          <h1 className="DetailTours_h1 col-12 mb-3">{tours.title}</h1>
-          <img
-            src={tours.image}
-            className="col-sm-12 col-xl-6"
-            alt="image tours"
-          />
-          <p className="col-sm-12 col-xl-6">{tours.description}</p>
-          <p className="col-6">
-            {tours.days} jours {tours.price} €{" "}
-          </p>
-          <p className="col-12 font-weight-bold">
-            Les destinations que vous allez decouvrir
-          </p>
-          {tours.destinations &&
-            tours.destinations.map((destination) => (
-              <div key={destination.id} className="mt-3 col-12">
-                <p className="font-weight-bold">{destination.city}</p>
-                <p>{destination.title}</p>
-                <p>{destination.description}</p>
-              </div>
-            ))}
-        </div>
+        {!loading && (
+          <div className="row">
+            <h1 className="DetailTours_h1 col-12 mb-3">{tours.title}</h1>
+            <img
+              src={tours.image}
+              className="col-sm-12 col-xl-6"
+              alt="image tours"
+            />
+            <p className="col-sm-12 col-xl-6">{tours.description}</p>
+            <p className="col-6">
+              {tours.days} jours {tours.price} €{" "}
+            </p>
+            <p className="col-12 font-weight-bold">
+              Les destinations que vous allez decouvrir
+            </p>
+            {tours.destinations &&
+              tours.destinations.map((destination) => (
+                <div key={destination.id} className="mt-3 col-12">
+                  <p className="font-weight-bold">{destination.city}</p>
+                  <p>{destination.title}</p>
+                  <p>{destination.description}</p>
+                </div>
+              ))}
+          </div>
+        )}
+        {loading && <ImageGrid />}
+
         <form className="row" onSubmit={handleSubmit}>
           <p className="col-12 font-weight-bold">
             Choisissez la date de votre départ
